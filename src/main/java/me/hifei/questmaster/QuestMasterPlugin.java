9e1d3cae6a4e0f84759d3e5a13c00497343f7567
BuildTools <unconfigured@null.spigotmc.org>
2023-08-06 20:46:10 +0800

    remove disable contents

--- src/main/java/me/hifei/questmaster/QuestMasterPlugin.java
+++ src/main/java/me/hifei/questmaster/QuestMasterPlugin.java
@@ -47,16 +47,5 @@ public class QuestMasterPlugin extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // todo: GameSavingCore.save();
-        for (QuestTeam team : CoreManager.manager.getTeams()) {
-            {
-                team.clear();
-                for (Quest q : team.getQuests()) {
-                    q.drop();
-                }
-            }
-        }
-        DynamicPanel.clear();
-        if (CoreManager.isGameStart()) CoreManager.game.drop();
     }
 }
