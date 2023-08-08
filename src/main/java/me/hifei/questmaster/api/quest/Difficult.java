3ac565b842bc600796d701d7c2be2f99f21a5b18
BuildTools <unconfigured@null.spigotmc.org>
2023-08-08 14:11:16 +0800

    change difficult again

--- src/main/java/me/hifei/questmaster/api/quest/Difficult.java
+++ src/main/java/me/hifei/questmaster/api/quest/Difficult.java
@@ -3,11 +3,11 @@ package me.hifei.questmaster.api.quest;
 import me.hifei.questmaster.running.config.Message;
 
 public enum Difficult {
-    EASY(Message.get("difficult.easy"), 1, 50),
-    NORMAL(Message.get("difficult.normal"), 50, 300),
-    HARD(Message.get("difficult.hard"), 300, 1000),
-    VERY_HARD(Message.get("difficult.very_hard"), 1000, 2000),
-    HELL(Message.get("difficult.hell"), 2000, 4000)
+    EASY(Message.get("difficult.easy"), 20, 150),
+    NORMAL(Message.get("difficult.normal"), 150, 500),
+    HARD(Message.get("difficult.hard"), 500, 1500),
+    VERY_HARD(Message.get("difficult.very_hard"), 1500, 4000),
+    HELL(Message.get("difficult.hell"), 4000, 10000)
     ;
     public final String name;
     public final double start;
