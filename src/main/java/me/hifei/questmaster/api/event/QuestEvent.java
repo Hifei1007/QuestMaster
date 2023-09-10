e45d4056b459de80e0eeec9f748010b9676964b9
Hifei1007 <46664548@qq.com>
2023-09-10 15:52:33 +0800

    1.2#39 [FIX] Load Settings method doesn't use custom deserializer

--- src/main/java/me/hifei/questmaster/api/event/QuestEvent.java
+++ src/main/java/me/hifei/questmaster/api/event/QuestEvent.java
@@ -1,10 +1,15 @@
 package me.hifei.questmaster.api.event;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import me.hifei.questmaster.QuestMasterPlugin;
 import me.hifei.questmaster.api.state.Stateful;
+import me.hifei.questmaster.running.gsoncfg.deserializer.DoubleBoundDeserializer;
+import me.hifei.questmaster.running.gsoncfg.deserializer.IntegerBoundDeserializer;
 import me.hifei.questmaster.running.gsoncfg.event.SingleEventConfig;
+import me.hifei.questmaster.running.gsoncfg.rolling.DoubleBoundConfig;
+import me.hifei.questmaster.running.gsoncfg.rolling.IntegerBoundConfig;
 import org.bukkit.ChatColor;
 
 import java.util.List;
@@ -33,7 +38,10 @@ public abstract class QuestEvent implements Stateful {
     }
 
     public <T> T loadSettings(Class<T> clazz) {
-        Gson gson = new Gson();
+        GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(IntegerBoundConfig.class, new IntegerBoundDeserializer());
+        builder.registerTypeAdapter(DoubleBoundConfig.class, new DoubleBoundDeserializer());
+        Gson gson = builder.create();
         return gson.fromJson(settings, clazz);
     }
 }
