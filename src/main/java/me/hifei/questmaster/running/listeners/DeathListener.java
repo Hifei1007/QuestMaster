69ebe6507b5a3f9b427a41448dfb46479c5ce43c
Hifei1007 <46664548@qq.com>
2023-09-10 16:34:31 +0800

    1.2#40 [FEA] Resistance on respawn

--- src/main/java/me/hifei/questmaster/running/listeners/DeathListener.java
+++ src/main/java/me/hifei/questmaster/running/listeners/DeathListener.java
@@ -1,5 +1,6 @@
 package me.hifei.questmaster.running.listeners;
 
+import me.hifei.questmaster.QuestMasterPlugin;
 import me.hifei.questmaster.api.CoreManager;
 import me.hifei.questmaster.running.config.Message;
 import me.hifei.questmaster.tools.ActionTool;
@@ -9,6 +10,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class DeathListener implements Listener {
     @EventHandler
@@ -25,5 +30,14 @@ public class DeathListener implements Listener {
                     })
             );
         }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        new BukkitRunnable() {
+            public void run() {
+                event.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 4, false, false, false));
+            }
+        }.runTask(QuestMasterPlugin.instance);
     }
 }
